c75a966c13c9759ebb552a046b636df1e6a54653
yongjin <yonggartong@naver.com>
2022-05-19 11:59:37 +0900

    프로젝트마무리

--- client/src/Component_Junehwan/Myreview.js
+++ client/src/Component_Junehwan/Myreview.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Myreview({ userInfo }) {
-  const [myReviews, setMyReviews] = useState([{ title: "", comment: "", updatedAt: "" }]);
+  const [myReviews, setMyReviews] = useState([{ movie_id: "", title: "", comment: "", updatedAt: "" }]); //영화정보도 필요하다.
+  const [moviePhotos, setMoviePhotos] = useState({ id: "", photo: "" });
 
   useEffect(() => {
     axios.get(`http://localhost:4000/setflix/reviews/${userInfo.id}`).then((res) => setMyReviews(res.data));
+    axios
+      .get(`http://localhost:4000/setflix/movies`)
+      .then((res) => res.data)
+      .then((photo) => setMoviePhotos(photo));
   }, []);
-
+  console.log(moviePhotos);
   console.log(myReviews);
 
   //   const review = myReviews.map();
@@ -18,7 +23,8 @@ export default function Myreview({ userInfo }) {
       ) : (
         myReviews.map((review) => (
           <div key={review.id}>
-            <img className="movie-pic" />
+            <img className="movie-pic" src={`https://image.tmdb.org/t/p/w92/8dzKn3RtPWUJRG9ymSpi423eMNV.jpg`} />
+            {/* review.movie_id로 movie테이블이미지를가져와야한다. */}
             <div>{review.title}</div>
             <div>{review.comment}</div>
             <div>{review.updatedAt}</div>
--- client/src/Componet_Soonkyu/list.js
+++ client/src/Componet_Soonkyu/list.js
@@ -2,51 +2,49 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Movielist from "../Movie/Movielist";
 import { Link } from "react-router-dom";
-import './list.css';
+import "./list.css";
 import sampledata from "./sample.json";
 
-function handleMovie( ) {
-    const [movieData, setMovieData] = useState([]);
-    const imageUrl = "https://image.tmdb.org/t/p/w94";
+function handleMovie() {
+  const [movieData, setMovieData] = useState([]);
+  const imageUrl = "https://image.tmdb.org/t/p/w154";
 
-    useEffect( () => {
-        async function fetchData() {
-            const res = await axios.get("http://localhost:4000/setflix/movies")
-            .then((res) => setMovieData(res.data))
-            const Movielist = res.movies.map((Movie) => (
-                {
-                    title: Movie.title,
-                    image: Movie.photo,
-                    releasedAtL: Movie.releasedAt,
-                    description: Movie.description
-                })
-            )
-            setMovieData(movieData.concat(Movielist))
-        } fetchData();
-    },[])
-    
-    console.log("movieDate", movieData);
-    console.log("더미", sampledata);
-    console.log("1값", movieData[1]);
-    
-    // console.log(movies);
+  useEffect(() => {
+    async function fetchData() {
+      const res = await axios.get("http://localhost:4000/setflix/movies").then((res) => setMovieData(res.data));
+      const Movielist = res.movies.map((Movie) => ({
+        title: Movie.title,
+        image: Movie.photo,
+        releasedAtL: Movie.releasedAt,
+        description: Movie.description,
+      }));
+      setMovieData(movieData.concat(Movielist));
+    }
+    fetchData();
+  }, []);
 
-    return (
-        <div className="listall">
-                {movieData.map(Movie => (
-                    <div className="list_movie">
-                        <div className="list_title">
-                            title : {Movie.title}
-                            <p>description : {Movie.description}</p>
-                        </div>
-                        `${imageUrl}{Movie.photo}`
-                    </div>
-            <Link to='/postreview'>
-            <button>후기 쓰기</button>
-            </Link>
-                ))}
+  console.log("movieDate", movieData);
+  console.log("더미", sampledata);
+  console.log("1값", movieData[1]);
+
+  // console.log(movies);
+
+  return (
+    <div className="listall">
+      {movieData.map((Movie) => (
+        <div className="list_movie">
+          <div className="list_title">
+            title : {Movie.title}
+            <p>description : {Movie.description}</p>
+          </div>
+          <img src={`${imageUrl}${Movie.photo}`} alt="My Image" />
         </div>
-    )
+      ))}
+      <Link to="/postreview">
+        <button>후기 쓰기</button>
+      </Link>
+    </div>
+  );
 }
 
 // <img className="list_image" src={Movie.photo} alt="">image :</img>
