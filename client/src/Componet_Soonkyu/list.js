13fe2dca7ca0ee81dc59e798e36fa34b6bc69756
sooonkyu <soonkyu602@gmail.com>
2022-05-19 11:41:55 +0900

    0519merge

--- client/src/Componet_Soonkyu/list.js
+++ client/src/Componet_Soonkyu/list.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import Movielist from "../Movie/Movielist";
 import { Link } from "react-router-dom";
 import './list.css';
 import sampledata from "./sample.json";
+import GridCars from "./GridCards";
+import { Row } from 'antd';
 
 function handleMovie( ) {
     const [movieData, setMovieData] = useState([]);
-    const imageUrl = "https://image.tmdb.org/t/p/w94";
+    const imageUrl = "https://image.tmdb.org/t/p/";
+    //const images = `${imageUrl}w500${Movie.photo}`
 
     useEffect( () => {
         async function fetchData() {
@@ -28,28 +30,47 @@ function handleMovie( ) {
     console.log("movieDate", movieData);
     console.log("더미", sampledata);
     console.log("1값", movieData[1]);
+    console.log("URL", imageUrl);
+
     
     // console.log(movies);
 
     return (
-        <div className="listall">
-                {movieData.map(Movie => (
-                    <div className="list_movie">
-                        <div className="list_title">
-                            title : {Movie.title}
-                            <p>description : {Movie.description}</p>
-                        </div>
-                        `${imageUrl}{Movie.photo}`
-                    </div>
+        <div className="listall" style={{ width: '85%', margin: '1rem auto'}}>
+            <Row gutter={[16, 16]}>
+                {movieData && movieData.map((Movie, index) => (
+                    <React.Fragment key={index}>
+                            <GridCars
+                                image={`${imageUrl}w500${Movie.photo}`}
+                                title={Movie.title}
+                                movieid={Movie.id}
+                            />
+                    </React.Fragment>
+                ))}           
             <Link to='/postreview'>
             <button>후기 쓰기</button>
             </Link>
-                ))}
+            </Row>
         </div>
     )
 }
 
 // <img className="list_image" src={Movie.photo} alt="">image :</img>
 //`${imageUrl}{Movie.photo}` 이미지 url 불러오기
+
+/*
+        <div className="listall">
+                {movieData.map(Movie => (
+                    <div className="list_movie">
+                        <div className="list_image>">
+                            <Link to='/postreview'><img src={`${imageUrl}w500${Movie.photo}`} alt={Movie.id}/></Link>
+                        </div>
+                        <div className="list_title">
+                            title : {Movie.title}
+                            <p>description : {Movie.description}</p>
+                        </div>
+                    </div>
+                ))}           
+            */
 
 export default handleMovie;
